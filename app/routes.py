26d905e678c72100a786e8510b78c831e09d1d4b
Maaz Ghani <maazghani@gmail.com>
2023-10-11 16:21:07 +0000

    adds the returning of three verses wip

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for
+from app import app
 import requests
 import json
-from app import app
+from flask import render_template, request, redirect, url_for
 
-# Replace BIBLE_ID with the specific Bible version ID you want to use
-BIBLE_ID = "de4e12af7f28f599-02"
+BIBLE_IDS = ["9879dbb7cfe39e4d-02", "65eec8e0b60e656b-01", "7142879509583d59-01"]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    api_key = "ba85c47cb54b17cf4016e5c62ef0bb4c"
+    headers = {'api-key': api_key}
+
+    # Fetch the list of available books for the first Bible ID as a sample
+    books_url = f"https://api.scripture.api.bible/v1/bibles/{BIBLE_IDS[0]}/books"
+    books_response = requests.get(books_url, headers=headers)
+    books_data = json.loads(books_response.text)
+    available_books = [book['name'] for book in books_data['data']]
+
     if request.method == 'POST':
+        selected_bible_id = request.form['bible_id']
         book = request.form['book']
         chapter = request.form['chapter']
         verse = request.form['verse']
-        bible_verse = fetch_bible_verse(book, chapter, verse)
-        return render_template('result.html', bible_verse=bible_verse)
-    return render_template('index.html')
 
-@app.route('/result')
-def result():
-    return render_template('result.html')
+        # Fetch the verse for each Bible ID
+        verse_data = {}
+        for bible_id in BIBLE_IDS:
+            verse_url = f"https://api.scripture.api.bible/v1/bibles/{bible_id}/verses/{book}.{chapter}.{verse}"
+            verse_response = requests.get(verse_url, headers=headers)
+            if verse_response.status_code == 200:
+                verse_json = json.loads(verse_response.text)
+                verse_data[bible_id] = verse_json['data']['content']
+            else:
+                verse_data[bible_id] = "Verse not found"
 
-def fetch_bible_verse(book, chapter, verse):
-    api_key = "ba85c47cb54b17cf4016e5c62ef0bb4c"
-    headers = {'api-key': api_key}
-    bibleVerseID = f"{book}.{chapter}.{verse}"
-    api_url = f"https://api.scripture.api.bible/v1/bibles/{BIBLE_ID}/verses/{bibleVerseID}?include-chapter-numbers=false&include-verse-numbers=false"
-    response = requests.get(api_url, headers=headers)
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        return data['data']['content']
-    else:
-        return "Verse not found"
+        return render_template('result.html', verse_data=verse_data)
+
+    return render_template('index.html', available_books=available_books)
